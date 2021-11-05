d7be2df0b05d5d49cc4d838897a7a28822a00d63
Mikulitch <60520096+Mikulitch@users.noreply.github.com>
2021-11-06 00:47:25 +0300

    Delete E-BookLibrary directory

--- E-BookLibrary/src/by/epam/group34/mikulich/e_library/entity/Roles.java
+++
@@ -1,6 +0,0 @@
-package by.epam.group34.mikulich.e_library.entity;
-
-public enum Roles {
-    ADMIN,
-    USER
-}
--- E-BookLibrary/src/by/epam/group34/mikulich/e_library/service/UserService.java
+++
@@ -1,4 +0,0 @@
-package by.epam.group34.mikulich.e_library.service;
-
-public interface UserService {
-}
